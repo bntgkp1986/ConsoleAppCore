ccd1f19dac1d86eb3055ba1a110b6f29e3714e28
bntgk <bntgk@DESKTOP-SFVBHQB>
2021-06-28 14:08:25 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ParentClass
+    { 
+    
+    }
+
+    public class DerivedClass
+    {
+        string name;
+        public DerivedClass(string name)
+        {
+            this.name = name;
+            Console.WriteLine("Ctor - " + this.name);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(this.name);
+        }
+    }
+     
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            DerivedClass obj = new DerivedClass("Brajesh Tripathi");
+            obj.Print();
+            Console.ReadLine();
+        }
+    }
+}
